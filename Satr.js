525581925b97ee232851279fcc10abace7059544
Saeed Al Berzawi <saidberzawi@gmail.com>
2023-04-19 14:45:22 +0300

    more add

---
+++ Satr.js
@@ -0,0 +1,25 @@
+const average = (array) => {
+  let numberOfElements = array.length;
+  let total = 0;
+
+  for (i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  let result = total / numberOfElements;
+  return result;
+};
+
+array = [2, 4, 5, 5, 7, 0];
+
+console.log(average(array));
+
+// ---------------------------------
+
+const Item = (arr,item,item2) => {
+ arr.push(item,item2);
+ arr.shift();
+ arr.shift();
+ return arr 
+};
+
+console.log(Item(array,2,5));
