0c851d74f7b03ddd96bfc143d31ad7576dade101
Saeed Al Berzawi <saidberzawi@gmail.com>
2023-07-01 11:53:47 +0300

    more add

--- Regex_CodeAcademy.js
+++ Regex_CodeAcademy.js
@@ -55,3 +55,98 @@ let quoteSample02 = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/gi;
 let result08 = quoteSample02.match(myRegex);
 console.log(result08);
+
+// => ^
+// negated character sets.
+// To create a negated character set, you place a caret character (^)
+// after the opening bracket and before the characters you do not want to match.
+// For example, /[^aeiou]/gi matches all characters that are not a vowel.
+let quoteSample03 = "3 blind mice.";
+let myRegex01 = /[^1-9^aioue]/gi; // Change this line
+let result09 = quoteSample03.match(myRegex01);
+console.log(result09);
+
+// => +
+// For example, /a+/g would find one match in abc and return ["a"].
+// Because of the +, it would also find a single match in aabc and return ["aa"].
+let difficultSpelling = "Mississippi";
+let myRegex02 = /s+/g; // Change this line
+let result010 = difficultSpelling.match(myRegex02);
+console.log(result010);
+
+// => *
+// matches characters that occur zero or more times
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+console.log(soccerWord.match(goRegex));
+console.log(gPhrase.match(goRegex));
+console.log(oPhrase.match(goRegex));
+
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+let chewieRegex = /Aa*/;
+let result011 = chewieQuote.match(chewieRegex);
+console.log(result011);
+
+// => ?
+// lazy matching.
+//  you can use the ? character to change it to lazy matching.
+//"titanic" matched against the adjusted regex of /t[a-z]*?i/ returns ["ti"].
+let text = "<h1>Winter is coming</h1>";
+let myRegex03 = /<.*?>/; // Change this line
+let result012 = text.match(myRegex03);
+console.log(result012);
+
+// ===========
+// Match Beginning String Patterns
+let firstString = "Ricky is first and can be found.";
+let firstRegex = /^Ricky/;
+console.log(firstRegex.test(firstString));
+let notFirst = "You can't find Ricky now.";
+console.log(firstRegex.test(notFirst));
+// The first test call would return true, while the second would return false.
+
+// => $
+// You can search the end of strings using the dollar sign character $ at the end of the regex.
+
+let theEnding = "This is a never ending story";
+let storyRegex = /story$/;
+console.log(storyRegex.test(theEnding));
+let noEnding = "Sometimes a story will have to end";
+console.log(storyRegex.test(noEnding));
+// The first test call would return true, while the second would return false.
+
+// => \w
+// Match All Letters and Numbers
+// The closest character class in JavaScript to match the alphabet is \w. This shortcut is equal to [A-Za-z0-9_].
+//This character class matches upper and lowercase letters plus numbers.
+//Note, this character class also includes the underscore character (_).
+//  /[A-Za-z0-9_]+/ => /\w+/
+
+let quoteSample04 = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g;
+let result013 = quoteSample.match(alphabetRegexV2).length;
+console.log(result013);
+
+// => \W
+// Match Everything But Letters and Numbers
+// the opposite pattern uses a capital letter. This shortcut is the same as [^A-Za-z0-9_].
+let shortHand = /\W/;
+let numbers = "42%";
+let sentence = "Coding!";
+console.log(numbers.match(shortHand));
+console.log(sentence.match(shortHand));
+// The first match call would return the value ["%"] and the second would return ["!"].
+
+let quoteSample05 = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g;
+let result014 = quoteSample05.match(nonAlphabetRegex).length;
+console.log(result014);
+let result015 = quoteSample05.match(nonAlphabetRegex);
+console.log(result015);
+
+// \d
+// Match All Numbers
+// The shortcut to look for digit characters is \d, with a lowercase d. This is equal to the character class [0-9],
+//which looks for a single character of any number between zero and nine.
